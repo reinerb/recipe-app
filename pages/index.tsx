f9d151a0a4d7c7e24a8a194b1f6c177306154760
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-05-03 17:26:59 -0400

    Add uuid and use for recipe IDs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import RecipeDisplay from '@/components/RecipeDisplay';
 import PrimaryLayout from '@/components/layouts/PrimaryLayout';
-import { randomUUID } from 'crypto';
+import { v4 as uuid } from 'uuid';
 
 const sampleRecipe = {
-  id: randomUUID(),
+  id: uuid(),
   title: 'Tolfredo',
   servings: 4,
   description: 'Crispy tofu and roasted broccoli with alfredo sauce',
