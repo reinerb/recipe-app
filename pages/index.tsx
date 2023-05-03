301156dbe2554a21165951a4c66f21460a2eb71c
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-05-03 11:06:55 -0400

    Add PrimaryLayout to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import PrimaryLayout from '@/components/layouts/PrimaryLayout';
+
 export default function Home() {
-  return <></>;
+  return (
+    <PrimaryLayout pageTitle='Recipe App'>
+      <h1>Hello world!</h1>
+    </PrimaryLayout>
+  );
 }
