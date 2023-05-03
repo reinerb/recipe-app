ff27ff08f39a7662c430f09bddb5015597e33469
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-05-03 11:06:31 -0400

    Add page title to PrimaryLayout Head

--- components/layouts/PrimaryLayout.tsx
+++ components/layouts/PrimaryLayout.tsx
@@ -1,16 +1,21 @@
 import React, { ReactNode } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
+import Head from 'next/head';
 
 interface Props {
+  pageTitle: string;
   children?: ReactNode;
 }
 
 function PrimaryLayout(props: Props) {
-  const { children } = props;
+  const { pageTitle, children } = props;
 
   return (
     <div>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
