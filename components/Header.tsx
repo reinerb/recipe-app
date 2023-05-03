11a28702ce83a17c0e920301ab1243cf491debc1
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-05-03 11:50:00 -0400

    Style header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import Link from 'next/link';
 
 interface Props {}
 
 function Header(props: Props) {
   const {} = props;
 
-  return <header></header>;
+  return (
+    <header className='flex flex-col items-center gap-1 bg-amber-200 px-8 py-4 dark:bg-amber-800 md:flex-row md:justify-between'>
+      <Link
+        href='/'
+        id='logo'
+        className='flex items-center gap-3'
+      >
+        <FontAwesomeIcon
+          className='text-2xl'
+          icon={faUtensils}
+        />
+        <span className='text-3xl'>Recipe App</span>
+      </Link>
+      <nav>
+        <ul className='flex gap-5'>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
