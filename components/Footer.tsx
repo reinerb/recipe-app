59c266bb66605de6c7c81cb1f1df47460c934b81
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-05-03 13:42:12 -0400

    Add content and styles to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,11 @@ interface Props {}
 function Footer(props: Props) {
   const {} = props;
 
-  return <footer></footer>;
+  return (
+    <footer className='flex justify-center bg-amber-200 px-8 py-4 dark:bg-amber-800'>
+      <p>Copyright &copy; Benjamin Reiner, 2023.</p>
+    </footer>
+  );
 }
 
 export default Footer;
